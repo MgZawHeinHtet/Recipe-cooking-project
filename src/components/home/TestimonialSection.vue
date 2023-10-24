101768422b0b384a63b618d756422815bddfb030
MgZawHeinHtet <zawheinhtet1223@gmail.com>
2023-10-24 21:03:54 +0630

    adding-in-testimonial

--- src/components/home/TestimonialSection.vue
+++ src/components/home/TestimonialSection.vue
@@ -11,7 +11,23 @@
             <testimonial-card :review="review"></testimonial-card>
           </Slide>
         </Carousel>
+
+        <div class="space-y-12">
+        <h2 class="text-center text-4xl/10  font-bold uppercase tracking-wider text-slate-700">Join our eat well, be well <br> culture.</h2>
+        <p class="text-center tracking-wide text-lg text-slate-600">Stay up to date with our new opening,and give feed back to our service</p>
+        <form class="w-[75%] mx-auto" action="">
+            <div class="border-y-2 h-24 grid grid-cols-3 border-slate-700">
+                <div class="col-span-2 flex items-center justify-center border-r-2 border-slate-700">
+                    <input type="text" class="bg-slate-200 w-[90%] h-14 px-5" placeholder="Enter your Opinion">
+                </div>
+                <div class="flex items-center justify-center  border-slate-700">
+                    <button class="bg-green-500 w-[80%] h-14 text-base uppercase text-slate-100 hover:bg-green-600 transition-all duration-200">Send</button>
+                </div>
+            </div>
+        </form>
+        </div>
       </div>
+
     </section>
 </template>
 
